837a9ad2287507e42af4c4e86b2a145a386109fa
VictorFlorence <vuhuy25092004@gmail.com>
2025-07-22 22:25:06 +0700

    Initial backend project (SpringBoot)

---
+++ backend/teaching-schedule-management-system/src/main/java/com/example/tsmstlu/TsmsTluApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsmstlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmsTluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsmsTluApplication.class, args);
+	}
+
+}
---
+++ backend/teaching-schedule-management-system/src/test/java/com/example/tsmstlu/TsmsTluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsmstlu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsmsTluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f93d7b0b7a8dd662c6c1278a34ab9610bceb6f28
VictorFlorence <vuhuy25092004@gmail.com>
2025-08-03 10:05:16 +0700

    Add initial project structure and configuration files for Flutter application

--- backend/teaching-schedule-management-system/src/main/java/com/example/tsmstlu/utils/MapperUtils.java
+++ backend/teaching-schedule-management-system/src/main/java/com/example/tsmstlu/utils/MapperUtils.java
@@ -3,7 +3,6 @@ package com.example.tsmstlu.utils;
 import com.example.tsmstlu.models.SemesterEntity;
 import com.example.tsmstlu.dtos.semester.SemesterDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MapperUtils {

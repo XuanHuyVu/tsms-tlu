ed60d69ac9b6309126b102cbecffa7924c8799af
HienLuong <luongdo2709@gmail.com>
2025-08-16 11:48:08 +0700

    feat(security): update CORS configuration to allow all origins

--- backend/teaching-schedule-management-system/src/main/java/com/example/tsmstlu/config/SecurityConfig.java
+++ backend/teaching-schedule-management-system/src/main/java/com/example/tsmstlu/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000, http://10.0.2.2"));
+        //config.setAllowedOrigins(List.of("http://localhost:3000", "http://10.0.2.2"));
+        config.addAllowedOriginPattern("*");
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
